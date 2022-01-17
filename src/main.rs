61a89b5a15f6b6a4ce35515f308339023712cdc2
SirAlienTheGreat <79958059+SirAlienTheGreat@users.noreply.github.com>
2022-01-17 16:13:58 -0700

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,253 @@
-use std::fs;
-use std::fs::File;
-use std::io::Read;
-use std::io;
-use std::error::Error;
-use csv::Writer;
+use fltk::{app,prelude::*,window::Window};
+use fltk::button::Button;
+use fltk::frame::Frame;
 
-fn output_as_csv(vector1:Vec<i32>, vector2:Vec<i64>) -> Result<(), Box<dyn Error>> {
-    assert_eq!(vector1.len(),vector2.len());
+fn calculate(label:&mut fltk::frame::Frame) {
+    let mut input =label.label().parse::<String>().unwrap();
 
-    let mut wtr = Writer::from_path("output.csv")?;
+    //remove whitespace
+    input = input.replace(" ",&"".to_string());
+    input = input.replace("\n",&"".to_string());
+    input = input.replace("-",&"+-".to_string());
 
-    for i in 0..vector1.len(){
-        println!("{}",vector1[i]);
-        wtr.write_record([
-            [&vector1[i].to_string()[..4],"-",&vector1[i].to_string()[4..] ].join(""),
-            vector2[i].to_string()]);
+    let operators = "+-*/";
+    let mut terms_string:Vec<String>= Vec::new();
+    let mut terms_float:Vec<f64>= Vec::new();
+    let mut operator_after:Vec<char>= Vec::new();
+    let mut key = 0;
+
+    // IE 4+9 would be converted to:
+        //  terms_float: [4,9]
+        //operator_after [+]
+
+    for i in input.chars() { //every character
+        if operators.contains(i){ //if i is an operator
+            operator_after.push(*&i);
+            key+=1;
+        } else if terms_string.len()<=key{ //if i is the first character in the term
+            terms_string.push(i.to_string());
+        } else{ //if i the second (or later) character in the term
+            terms_string[key].push(*&i);
+        }
     }
 
+    for i in &terms_string{ //convert strings to float
+        let x: f64 = i.parse().unwrap();
+        terms_float.push(x);
+    }
 
-    wtr.flush()?;
-    Ok(())
+    let mut x:f64 = 0.0;
+    while x <operator_after.len() as f64  { //Multiplication and division
+        let i = x as usize;
+        if operator_after[i]=='*'{
+            terms_float[i] = terms_float[i]*terms_float[i+1];
+            terms_float.remove(i+1);
+            operator_after.remove(i);
+        }else if operator_after[i]=='/' {
+            terms_float[i] = terms_float[i] / terms_float[i + 1];
+            terms_float.remove(i + 1);
+            operator_after.remove(i);
+        } else{
+            x += 1.0;
+        }
+    }
+
+    x = 0.0;
+    while x <operator_after.len() as f64  { //Addition and Subtraction
+        let i = x as usize;
+        if operator_after[i]=='+'{
+            terms_float[i] = terms_float[i]+terms_float[i+1];
+            terms_float.remove(i+1);
+            operator_after.remove(i);
+        }else if operator_after[i]=='-'{
+            terms_float[i] = terms_float[i]-terms_float[i+1];
+            terms_float.remove(i+1);
+            operator_after.remove(i);
+        }else{
+            x += 1.0;
+        }
+    }
+    let output = terms_float[0];
+    label.set_label(&output.to_string());
+}
+
+fn add_to_text(character:char, label:&mut fltk::frame::Frame){
+    let mut text =label.label().parse::<String>().unwrap();
+    text.push(character);
+    label.set_label(&text);
+    if text.len()>=9 &&text.len()<12 {
+        label.set_label_size(35);
+    }else if text.len() >= 12{
+        label.set_label_size(30);
+    }
+}
+
+fn backspace(label:&mut fltk::frame::Frame){
+    let mut text =label.label().parse::<String>().unwrap();
+    text.pop();
+    label.set_label(&text);
+    label.set_label_size(45);
 }
 
 fn main() {
-    println!("\n\n      Discord GDPR data grapher by Allen MacFarland\
-    \nRequest your discord GDPR data in Discord > Settings > Privacy and security > Request data\
-    \nYou will receive an email containing a .zip file with all your data\
-    \nExtract the messages folder from this .zip file\
-    \nEnter a path to the .zip file\
-    \nThis can be an:\
-    \n* Absolute path like /run/media/sir-alien-the-great/Barracuda 4TB/Discord GDPR data/messages\
-    \n* Relative path like Discord GDPR data/messages\
-    \n* Relative path that goes backwards like ../Discord GDPR data/messages");
+    let equation = "2+2+2+2".to_string();
 
-    let mut input = "".to_string();
-    io::stdin().read_line(&mut input).expect("A problem occurred");
-
-    let paths = fs::read_dir(input.trim()).unwrap();
-    let mut str_path :Vec<String> = vec![];
-    let n = 7;
+    //window variables
+    let text_box_size = 70;
+    let buffer = 20;
+    let button_size = 50;
+    let button_font_size = 20;
 
 
-    for path in paths { // Convert path (:ReadDir) to :String
-        if !path.as_ref().unwrap().path().display().to_string().contains("index"){
-            str_path.push([path.unwrap().path().display().to_string(),"/messages.csv".to_string()].join(""))
-        }
+    let app = app::App::default()
+        .with_scheme(app::Scheme::Gtk);
 
-    }
+    let mut windowobj = Window::new(400,0,6*button_size+7*buffer,3*button_size+4*buffer+text_box_size,"Calculator");
 
+    let _frame = Frame::default()
+        .center_of(&windowobj)
+        .size_of(&windowobj);
 
-    let mut dates:Vec<String> = vec![];
-    let mut messages:Vec<String> = vec![];
-
-
-    for path in str_path { // For every messages file:
-
-        // Read CSV file as string
-        let mut file = File::open(path)
-            .expect("Why are you trying to break my program?\nWhy do you hate me?");
-        let mut randomstring = String::new();
-        file.read_to_string(&mut randomstring)
-            .expect("You picked the wrong path because you wanted to see my program fail, didn't you?");
-
-        // We only care about the 2nd and 3rd row of each csv, so write new vectors with just that
-        let mut comma_counter = 5;
-        let mut in_quotes = false;
-        let mut current_date = "".to_string();
-        let mut current_string = "".to_string();
-
-        for c in randomstring.chars(){ // For every character:
-            if comma_counter == 0 {
-                if c != ',' || in_quotes {
-                    if c == '"'{
-                        if !in_quotes{
-                            in_quotes = true;
-                        } else {
-                            in_quotes = false;
-                        }
-                    } else {
-                        current_string.push(c);
-                    }
-                }else{
-                    comma_counter = 2;
-                    dates.push(current_date);
-                    current_date = "".to_string();
-
-                    messages.push(current_string);
-                    current_string = "".to_string();
-                }
-            } else if c != ',' && comma_counter ==1 {
-                current_date.push(c);
-            } else if c == ','{
-                comma_counter+=-1;
-            }
-        }
-    }
-
-    let mut months :Vec<i32> = vec![];
+    let mut text_label = Frame::default()
+        .with_size(250,text_box_size)
+        .with_pos(buffer,buffer)
+        .with_label(&equation);
+    text_label.set_label_size(45);
 
 
-    // create list of months
-    for date in &dates { //For every date
-        let x:i32 = date.chars().take(n).collect::<String>()
-            .replace("-","").parse().unwrap(); //example 202003 :i32
-        if !months.contains(&x) {
-            months.push(x);
-        }
-    }
+    // Top row buttons (1-5)
+    let mut but1 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer,text_box_size+buffer)
+        .with_label("1");
+    but1.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('1',&mut text_label)}});
+    but1.set_label_size(button_font_size);
 
-    // Bubble sort months
-    let mut sorted:bool=false;
-    let mut last_unsorted = months.len() - 1;
-    while !sorted {
-        sorted = true;
-        for i in 0..last_unsorted{
-            if months[i] > months[i+1]{
-                let erste = months[i];
-                let zweite = months[i+1];
-                months[i] = zweite;
-                months[i+1] =erste;
-                sorted = false;
-            }
-        }
-        last_unsorted = last_unsorted -1;
-    }
+    let mut but2 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*2+button_size,text_box_size+buffer)
+        .with_label("2");
+    but2.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('2',&mut text_label)}});
+    but2.set_label_size(button_font_size);
 
-    //Calculate the number of messages/characters sent in each month
-    let mut months_messages :Vec<i64> = vec![0;months.len().clone().try_into().unwrap()];
+    let mut but3 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*3+button_size*2,text_box_size+buffer)
+        .with_label("3");
+    but3.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('3',&mut text_label)}});
+    but3.set_label_size(button_font_size);
 
-    for key in 0..messages.len()-1 { //For every message
-        let date:&i32 = &dates[key].chars().take(n).collect::<String>()
-            .replace("-","").parse().unwrap();//example 202003 :i32
+    let mut but4 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*4+button_size*3,text_box_size+buffer)
+        .with_label("4");
+    but4.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('4',&mut text_label)}});
+    but4.set_label_size(button_font_size);
 
-        for i in 0..months.len(){ //For every month
-            if date == &months[i]{
-                months_messages[i]+=messages[key].len() as i64;
-            }
-        }
-    }
+    let mut but5 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*5+button_size*4,text_box_size+buffer)
+        .with_label("5");
+    but5.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('5',&mut text_label)}});
+    but5.set_label_size(button_font_size);
 
-    //Write to CSV
-    if let Err(err) = output_as_csv(months,months_messages){
-        println!("{}",err);
-    }
+
+    // Second row buttons (6-0)
+    let mut but6 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer,text_box_size+buffer*2+button_size)
+        .with_label("6");
+    but6.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('6',&mut text_label)}});
+    but6.set_label_size(button_font_size);
+
+    let mut but7 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*2+button_size,text_box_size+buffer*2+button_size)
+        .with_label("7");
+    but7.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('7',&mut text_label)}});
+    but7.set_label_size(button_font_size);
+
+    let mut but8 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*3+button_size*2,text_box_size+buffer*2+button_size)
+        .with_label("8");
+    but8.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('8',&mut text_label)}});
+    but8.set_label_size(button_font_size);
+
+    let mut but9 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*4+button_size*3,text_box_size+buffer*2+button_size)
+        .with_label("9");
+    but9.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('9',&mut text_label)}});
+    but9.set_label_size(button_font_size);
+
+    let mut but0 = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*5+button_size*4,text_box_size+buffer*2+button_size)
+        .with_label("0");
+    but0.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('0',&mut text_label)}});
+    but0.set_label_size(button_font_size);
+
+    // Bottom row buttons (Symbols)
+    let mut butplus = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer,text_box_size+buffer*3+button_size*2)
+        .with_label("+");
+    butplus.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('+',&mut text_label)}});
+    butplus.set_label_size(button_font_size);
+
+    let mut butminus = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*2+button_size,text_box_size+buffer*3+button_size*2)
+        .with_label("-");
+    butminus.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('-',&mut text_label)}});
+    butminus.set_label_size(button_font_size);
+
+    let mut buttimes = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*3+button_size*2,text_box_size+buffer*3+button_size*2)
+        .with_label("x");
+    buttimes.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('*',&mut text_label)}});
+    buttimes.set_label_size(button_font_size);
+
+    let mut butdivide = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*4+button_size*3,text_box_size+buffer*3+button_size*2)
+        .with_label("÷");
+    butdivide.set_callback({let mut text_label = text_label.clone(); move |_|{
+        add_to_text('/',&mut text_label)}});
+    butdivide.set_label_size(button_font_size);
+
+    let mut butbackspace = Button::default()
+        .with_size(button_size,button_size)
+        .with_pos(buffer*5+button_size*4,text_box_size+buffer*3+button_size*2)
+        .with_label("<");
+    butbackspace.set_callback({let mut text_label = text_label.clone(); move |_|{
+        backspace(&mut text_label)}});
+    butbackspace.set_label_size(button_font_size);
+
+    let mut butequals = Button::default()
+        .with_size(button_size,button_size*3+buffer*2)
+        .with_pos(buffer*6+button_size*5,text_box_size+buffer)
+        .with_label("=");
+    butequals.set_callback({let mut text_label = text_label.clone(); move |_| calculate(&mut text_label) });
+    butequals.set_label_size(button_font_size);
+
+    windowobj.make_resizable(true);
+    windowobj.end();
+    windowobj.show();
+    app.run().unwrap();
 }
